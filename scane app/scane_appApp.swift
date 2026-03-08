d90d3e65828f4394a9cd59cf17e5ab9dc07445ee
Sheikh Abu Mohammed <sheikh.mohammed+citrix@cloud.com>
2026-03-08 12:06:32 +0530

    Initial Commit

---
+++ scane app/scane_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  scane_appApp.swift
+//  scane app
+//
+//  Created by sheikh abu mohamed on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct scane_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
